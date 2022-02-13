808e4e0f1fed213dfe807b0a07b908adcdac2338
PRAM2844 <dolce3527@gmail.com>
2022-02-14 01:13:57 +0900

    create LoginScreen

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LoginScreen from './src/screens/LoginScreen';
+
+export default function App() {
+  return (
+    <LoginScreen />
+  );
+}
---
+++ src/screens/LoginScreen.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {
+  StyleSheet, Text, View, TextInput,
+} from 'react-native';
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.inner}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput style={styles.input} value="Email Adress" />
+        <TextInput style={styles.input} value="Password" />
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonLabel}>Submit</Text>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Not registered?</Text>
+          <Text style={styles.footerLink}>Sign up here!</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'skyblue',
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
+    backgroundColor: '#ffffff',
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  buttonContainer: {
+    backgroundColor: '#467FD3',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: '#ffffff',
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467FD3',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+});
